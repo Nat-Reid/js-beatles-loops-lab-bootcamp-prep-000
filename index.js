a0ffd25f19356b80da14d9654637ace3da462cda
Nat Reid <nathanielreid@gmail.com>
2018-10-13 22:29:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function theBeatlesPlay(musicians, instruments){
   var strings = [];
   for (let i=0; i<=musicians.length; i++){
-    strings.push(musicians[i]+'plays'+instruments[i]);
+    strings.push(musicians[i]+' plays '+instruments[i]);
   }
   return strings;
 }
